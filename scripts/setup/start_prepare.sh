68efa8f16a265367660fb9154c33f4165090c116
dezihh <44939692+dezihh@users.noreply.github.com>
2025-11-24 18:27:49 +0100

    Add start_prepare.sh for system setup
    This script prepares the system for printing by installing necessary packages and configuring user permissions.

---
+++ scripts/setup/start_prepare.sh
@@ -0,0 +1,14 @@
+echo "Bitte Script als Hauptuser ausführen!"
+echo -n "Checke Systemzeit: "
+date
+echo 'Systemzeit korrekt? Sonst sudo date -s "11/23/25 11:38:00"'
+echo -n 'Weiter? Abbruch mit Ctrl-C:'; read a
+
+sudo apt update
+sudo apt install -y cups avahi-daemon libnss-mdns printer-driver-all cups-bsd cups-client ipptool snmp snmp-mibs-downloader ntp
+echo "Trage $USER als LpAdmin ein..."
+sudo usermod -aG lpadmin $USER
+sudo systemctl enable --now cups avahi-daemon
+
+
+
