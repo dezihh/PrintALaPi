0e154bb249119c7f49b4ff91c95b22824c38a5c7
dezihh <44939692+dezihh@users.noreply.github.com>
2025-11-29 12:01:21 +0100

    Refactor service management in start_prepare.sh
    Removed unnecessary service stops and disables, and cleaned up commented code.

--- scripts/setup/start_prepare.sh
+++ scripts/setup/start_prepare.sh
@@ -13,15 +13,13 @@ wget -O /usr/lib/systemd/system/zram-setup.service https://raw.githubusercontent
 sudo systemctl daemon-reload
 sudo systemctl enable zram-setup.service
 sudo systemctl start zram-setup.service
-
-
 sudo loginctl disable-linger
 
 # Stop rw services
-sudo systemctl stop apt-daily.timer apt-daily-upgrade.timer man-db.timer e2scrub_all.timer fstrim.timer dpkg-db-backup.timer ModemManager polkit  systemd-journald systemd-timesyncd logrotate.timer logrotate.service
-sudo systemctl disable apt-daily.timer apt-daily-upgrade.timer man-db.timer e2scrub_all.timer fstrim.timer dpkg-db-backup.timer ModemManager polkit systemd-journald systemd-timesyncd logrotate.timer logrotate.service
+sudo systemctl stop  man-db.timer   dpkg-db-backup.timer ModemManager polkit logrotate.timer logrotate.service
+sudo systemctl disable man-db.timer   ModemManager polkit systemd-journald s logrotate.timer logrotate.service
 sudo loginctl disable-linger
-sudo systemctl mask systemd-journald user@1000.service
+sudo systemctl mask user@1000.service
 sudo usermod -aG lpadmin printalapi
 
 # Stop Logs beeing writed
@@ -37,8 +35,6 @@ ln -s /dev/null /var/log
 
 # Set our Cups config from github in place
 sudo wget -O /etc/cups/cupsd.conf https://raw.githubusercontent.com/dezihh/PrintALaPi/master/scripts/cupsd.conf
-sudo mv /etc/cups /etc/cups.backup
-ln -s /mnt/zram/cups/etc/ /etc/cups/
 sudo systemctl restart cups
 
 # Some helpful information at logintime
@@ -47,19 +43,6 @@ sudo wget -O /etc/motd https://raw.githubusercontent.com/dezihh/PrintALaPi/maste
 # No stay in homedir (important for RO Mode)
 sudo echo "# Prevent starting D-Bus" >>/etc/profile
 sudo echo "cd /tmp >/dev/null 2>&1 || true" >> /etc/profile
-
-# Set Filesystem to RO
-sudo sed -i 's/$/ rootflags=ro/' /boot/firmware/cmdline.txt
-sudo sed -i 's|^[^#].*/\s.*ext4.*defaults|&,ro|' /etc/fstab
-#sudo systemctl enable --now cups avahi-daemon
-#sudo wget -O /etc/cups/cupsd.conf "https://raw.githubusercontent.com/dezihh/PrintALaPi/master/scripts/cupsd.conf"
-#sudo systemctl restart cups
-# Drucker freigeben
-#sudo lpadmin -p LexmarkE330 -o printer-is-shared=true
-# AirPrint-kompatible MIME-Typen
-#sudo lpadmin -p LexmarkE330 -o printer-op-policy=default
-
-#sudo tee /usr/local/sbin/setup-zram-fs.sh >/dev/null << 'EOF'
 
 
 
