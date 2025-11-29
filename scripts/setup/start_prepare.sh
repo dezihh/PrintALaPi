0722da4e90a3707e67345564d50f5ba8329565e9
dezihh <44939692+dezihh@users.noreply.github.com>
2025-11-29 11:44:07 +0100

    Update start_prepare.sh

--- scripts/setup/start_prepare.sh
+++ scripts/setup/start_prepare.sh
@@ -13,47 +13,44 @@ wget -O /usr/lib/systemd/system/zram-setup.service https://raw.githubusercontent
 sudo systemctl daemon-reload
 sudo systemctl enable zram-setup.service
 sudo systemctl start zram-setup.service
-sudo systemctl stop ModemManager
-sudo systemctl disable ModemManager
-sudo systemctl stop polkit
-sudo systemctl disable polkit
+
+
+sudo loginctl disable-linger
+
 # Stop rw services
-sudo systemctl stop apt-daily.timer apt-daily-upgrade.timer man-db.timer e2scrub_all.timer fstrim.timer dpkg-db-backup.timer
-sudo systemctl disable apt-daily.timer apt-daily-upgrade.timer man-db.timer e2scrub_all.timer fstrim.timer dpkg-db-backup.timer
+sudo systemctl stop apt-daily.timer apt-daily-upgrade.timer man-db.timer e2scrub_all.timer fstrim.timer dpkg-db-backup.timer ModemManager polkit  systemd-journald systemd-timesyncd logrotate.timer logrotate.service
+sudo systemctl disable apt-daily.timer apt-daily-upgrade.timer man-db.timer e2scrub_all.timer fstrim.timer dpkg-db-backup.timer ModemManager polkit systemd-journald systemd-timesyncd logrotate.timer logrotate.service
+sudo loginctl disable-linger
+sudo systemctl mask systemd-journald user@1000.service
+sudo usermod -aG lpadmin printalapi
+
+# Stop Logs beeing writed
 echo "[Journal]
 Storage=none
 SystemMaxUse=1M
 ForwardToSyslog=no
 ForwardToWall=no" >/etc/systemd/journald.conf
 
-sudo systemctl stop systemd-journald
-sudo systemctl disable systemd-journald
-sudo systemctl mask systemd-journald
-
+# Remove old Log stuff and link Directory to /dev/null
 rm -rf /var/log
 ln -s /dev/null /var/log
 
-#sudo systemctl stop NetworkManager
-#sudo systemctl disable NetworkManager
-#sudo systemctl mask NetworkManager
-
-sudo systemctl stop systemd-timesyncd
-sudo systemctl disable systemd-timesyncd
-
-# 4. Logrotate deaktivieren (da wir keine Logs haben)
-sudo systemctl stop logrotate.timer
-sudo systemctl disable logrotate.timer
-sudo systemctl stop logrotate.service
-sudo systemctl disable logrotate.service
-
+# Set our Cups config from github in place
 sudo wget -O /etc/cups/cupsd.conf https://raw.githubusercontent.com/dezihh/PrintALaPi/master/scripts/cupsd.conf
+sudo mv /etc/cups /etc/cups.backup
+ln -s /mnt/zram/cups/etc/ /etc/cups/
 sudo systemctl restart cups
 
-sudo usermod -aG lpadmin printalapi
-
+# Some helpful information at logintime
 sudo wget -O /etc/motd https://raw.githubusercontent.com/dezihh/PrintALaPi/master/scripts/motd
+
 # No stay in homedir (important for RO Mode)
-sudo sh -c 'echo "cd /tmp >/dev/null 2>&1 || true" >> /etc/profile'
+sudo echo "# Prevent starting D-Bus" >>/etc/profile
+sudo echo "cd /tmp >/dev/null 2>&1 || true" >> /etc/profile
+
+# Set Filesystem to RO
+sudo sed -i 's/$/ rootflags=ro/' /boot/firmware/cmdline.txt
+sudo sed -i 's|^[^#].*/\s.*ext4.*defaults|&,ro|' /etc/fstab
 #sudo systemctl enable --now cups avahi-daemon
 #sudo wget -O /etc/cups/cupsd.conf "https://raw.githubusercontent.com/dezihh/PrintALaPi/master/scripts/cupsd.conf"
 #sudo systemctl restart cups
